2c8b7a61a8d8de34287614bc27a1da5585d8fe88
Emporio94 <94363091+Emporio94@users.noreply.github.com>
2024-03-14 16:15:29 +0000

    Update Bank.java

--- oose-lab-exam-bank-main/src/main/java/Bank.java
+++ oose-lab-exam-bank-main/src/main/java/Bank.java
@@ -41,7 +41,14 @@ public class Bank {
     protected void setUpPeople(){
         List<String[]> csvPeople = loadCSV.getCSVRows("src/main/resource/people.csv");
         for (String[] row : csvPeople){
-            //Task One
+            String role = row[0];
+            AssetType assetType = AssetType.valueOf(row[3]);
+            Person person = PersonFactory.createPerson(row);
+
+            TraderAssetDesk desk = getTraderAssetDesk(assetType);
+            if (desk != null) {
+                desk.addTrader(person);
+            }
         }
     }
 
